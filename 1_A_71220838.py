bed1b6d22564401f3cc9c7fabf15f075496ab38d
chikavenesa <118042097+chikavenesa@users.noreply.github.com>
2022-11-18 10:32:41 +0700

    Add files via upload

---
+++ 1_A_71220838.py
@@ -0,0 +1,5 @@
+masukkannamaAnda = input ('Masukkan nama Anda: ')
+masukkannamamatakuliah = input ('Masukkan nama matakuliah: ')
+masukkangrupAnda = input ('Masukkan grup Anda: ')
+print(f'Haloo! {masukkannamaAnda}{masukkannamaAnda}')
+print('Anda tergabung dalam ' + masukkannamamatakuliah + ' pada grup ' + masukkangrupAnda)
